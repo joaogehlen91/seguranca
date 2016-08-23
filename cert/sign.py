9bd390421581d296dd6b4ea1c5271e838650024b
João Gehlen <joaogehlen91@gmail.com>
2016-08-23 16:32:56 -0300

    assinatura digital e validacao funcionando

--- cert/sign.py
+++ cert/sign.py
@@ -13,9 +13,6 @@ hash.update(text)
 sha = hash.digest()
 
 
-text_sha = text + "\n" + sha
-
-
 """ gera chave publica e privada """
 random_generator = Random.new().read
 key = RSA.generate(2048, random_generator)
@@ -25,18 +22,17 @@ str_private_key = key.exportKey("PEM")
 #print(str_public_key)
 #print(str_private_key)
 
-print(sha+"\n")
-sha_encrypted = public_key.encrypt(sha, 32)
-print(type(sha_encrypted))
-
+#print('',sha+"\n")
+signature = public_key.encrypt(sha, 32)
+#print(type(signature))
 
 fkey = open('key.pem', 'w').write(str(str_public_key))
 
-fout = open('file_encrypted', 'w')
+fout = open('file_signed', 'w')
 fout.write(str(text))
 fout.write('\n')
 fout.write(str(sha))
 fout.write('\n')
-fout.write(str(sha_encrypted))
+fout.write(str(signature))
 fout.write('\n')
 fout.write(str(str_public_key))
--- cert/validate.py
+++ cert/validate.py
@@ -4,9 +4,26 @@ from Crypto import Random
 import sys
 
 
+#Chave publica
 fkey = open('key.pem', 'r')
+public_key = RSA.importKey(fkey.read())
 
 
-key = RSA.importKey(fkey.read())
+#Arquivo 
+file = open(sys.argv[1], 'r')
+list_text = file.readlines()
+text = list_text[0].replace('\n', '')
+signature_orig = list_text[2].replace('\n', '')
 
 
+hash = SHA256.new()
+hash.update(text)
+sha = hash.digest()
+
+signature = public_key.encrypt(sha, 32)
+
+
+if str(signature) == str(signature_orig):
+    print("Assinatura valida")
+else:
+    print("Assinatura invalida")
