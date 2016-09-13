d130624fce1404387de6b8a1903d6fb0d7897733
Joao Gehlen <joaogehlen91@gmail.com>
2016-09-13 15:32:36 -0300

    quebrar vigenere com texto claro e escuro

---
+++ classic/vigenere/quebrar/claro_escuro.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+f_claro = open(argv[1], 'rb')
+claro = f_claro.read()
+
+f_escuro = open(argv[2], 'rb')
+escuro = f_escuro.read()
+
+chave = ''
+for i, j in zip(claro, escuro):
+    chave += chr(j - i)
+
+print(chave)
