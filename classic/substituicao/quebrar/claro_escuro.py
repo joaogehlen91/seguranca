9bab30a60cd5664192aabfd0ef6856f4c0315da8
João Gehlen <joaogehlen91@gmail.com>
2016-09-13 20:43:10 -0300

    iniciando testes para quebrar substituicao

---
+++ classic/substituicao/quebrar/claro_escuro.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+f_claro = open(argv[1], 'rb')
+claro = f_claro.read()
+
+f_escuro = open(argv[2], 'rb')
+escuro = f_escuro.read()
+
+chave = {}
+for i, j in zip(claro, escuro):
+    chave.update({j:i})
+
+print(chave)
+
