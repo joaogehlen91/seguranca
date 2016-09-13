03924367d92a251537292195a34cf78bcced3cbd
João Gehlen <joaogehlen91@gmail.com>
2016-09-13 20:29:05 -0300

    ajustado cifra de substituição

--- classic/substituicao/substituicao.py
+++ classic/substituicao/substituicao.py
@@ -1,51 +1,30 @@
-chave_cifra = {
-    'a':'x',
-    'b':'d',
-    'c':'s',
-    'd':'k',
-    'e':'p',
-    'f':'i',
-    'g':'o',
-    'h':'q',
-    'i':'c',
-    'j':'t',
-    'k':'u',
-    'l':'e',
-    'm':'v',
-    'n':'j',
-    'o':'r',
-    'p':'a',
-    'q':'m',
-    'r':'g',
-    's':'z',
-    't':'y',
-    'u':'b',
-    'v':'w',
-    'w':'h',
-    'x':'l',
-    'y':'n',
-    'z':'f',
-    ' ':'_'
-}
-chave_decifra = {}
-for i in chave_cifra:
-    chave_decifra.update({chave_cifra[i]:i})
+from random import shuffle
+from sys import argv
+
+def inverte_chave(chave):
+    return dict([(j, i) for (i, j) in chave.items()])
+
+def gera_chave():
+    try:
+        file_key = open(argv[2], 'rb')
+        f = file_key.read()
+        key = dict(enumerate(f))
+    except Exception:
+        a = list(range(256))
+        shuffle(a)
+        key = dict(enumerate(a))
+    return key
 
 
-mensagem = "atacar a base sul"
+file = open(argv[1], 'rb')
+msg = file.read()
+key = gera_chave()
 
-#Cifra
-msg_cifrada = []
-for i in mensagem:
-    msg_cifrada.append(chave_cifra[i])
+#cifrar
+msg_cifrada = bytes(key[x] for x in msg)
+open('output_cifrado', 'wb').write(msg_cifrada)
 
-msg_cifrada = ''.join(msg_cifrada)
-print('Mensagem cifrada: ' + msg_cifrada)
-
-#Decifra
-msg_decifrada = []
-for i in msg_cifrada:
-    msg_decifrada.append(chave_decifra[i])
-
-msg_decifrada = ''.join(msg_decifrada)
-print('Mensagem original: ' + msg_decifrada)
+#decifra
+key = inverte_chave(key)
+msg_decifrada = bytes(key[x] for x in msg_cifrada)
+open('output_original', 'wb').write(msg_decifrada)
