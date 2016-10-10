1f5ef4989132aee0c90d561082efe130af6f15e0
Joao Gehlen <joaogehlen91@gmail.com>
2016-10-10 01:54:59 -0300

    ajuste para funcionar de vez cifra de transposicao

--- classic/transposicao/transposicao.py
+++ classic/transposicao/transposicao.py
@@ -1,32 +1,40 @@
 import numpy as np
+from math import ceil
+from itertools import zip_longest
+from sys import argv
 
 
-mensagem = "esta mensagem era pra ser um segredo"
-chave = 3
-som = chave
+def cifra(chave, data, name_file):
+    lista = []
+    matriz = []
+
+    for x in range(0, len(data), chave):
+        lista.append(data[x:x+chave])
+
+    z = zip_longest(*lista, fillvalue='#')
+
+    for i in z:
+        matriz.append(list(i))
+
+    matriz_t = np.array(matriz).tolist()
+
+    msg = [j for i in matriz_t for j in i]
+    msg_cifrada = ''.join(msg)
+
+    open(name_file, 'wb').write(bytes(msg_cifrada, 'utf-8'))
 
 
-l_mensagem = list(mensagem)
-for x in xrange(len(mensagem) % chave):
-    l_mensagem.append('#')
-
-matriz = []
-linha = []
-
-for x in xrange(len(l_mensagem)):
-    linha.append(l_mensagem[x])
-    if x == chave-1:
-        matriz.append(linha)
-        linha = []
-        chave += som
-
-#print(matriz)
-matriz_t = np.array(matriz)
-matriz_t = matriz_t.transpose().tolist()
-#print(matriz_t)
+def decifra(chave, data, name_file):
+    cifra(int(ceil(len(data) / chave)), data, name_file)
 
 
-#Formatar matriz para mostrar mensagem cifrada
-lista = [j for i in matriz_t for j in i]
-msg_cifrada = ''.join(lista)
-print("Mensagem cifrada:\n" + msg_cifrada+"\n")
+if len(argv) == 1:
+    print("Use 'python transposicao' [file input] [-c cifrar | -d decifrar] [chave]")
+else:
+    file = open(argv[1], 'r')
+    data = file.read()
+    chave = int(argv[3])
+    if argv[2] == '-c':
+        cifra(chave, data, 'output_cifrado')
+    else:
+        decifra(chave, data, 'output_original')
